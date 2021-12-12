a8a1a6499a84f2b1451fa0355f89ad3f5c8e4840
Salva Blanquer Bru <salvablanquer0@gmail.com>
2021-12-12 13:17:39 +0100

    change of API and it's working

--- HTML/main.js
+++ HTML/main.js
@@ -1,15 +1,48 @@
+function getApiSearch() {
 
-fetch("https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/inception", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
-		"x-rapidapi-key": "6e6a3540bdmshe654054b2e62cb3p16c003jsn31b831c7707d"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+    const valuesInput = document.getElementById('search-input').value;
 
+    const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=1e6296feeb7565b54f1f8ea079f7e70e&language=es&query=${valuesInput}`;
+
+    const miInit = { method: 'GET'};
+
+    fetch(apiUrl, miInit)
+
+    .then(response => response.json())
+
+    .then(data => {	
+
+        let filtradoSearch = data.results[0]
+
+        if ( filtradoSearch === undefined ) {
+
+            document.getElementById("renderizado-datos")
+            .innerHTML = `La pelicula no se encontro`;
+
+        } else {
+
+            const { title} = filtradoSearch;
+
+            document.getElementById("renderizado-datos")
+            .innerHTML = `
+            <style>
+            body::before {
+                content: '';
+                position: absolute;
+                left: 0;
+                top: 0;
+                width: 100%;
+                height: 100%;
+                z-index: -1;});
+            }
+
+            </style>
+
+            <div class="resultado-api">
+
+                <div class="datos-api"><h2>${title}</h2>
+					
+            </div>`;
+        }
+    })
+};
